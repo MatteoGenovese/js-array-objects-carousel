be5a797e7518b20601ae4d8c2fcb16d3b4901cde
MatteoGenovese <matteo.genovese@icloud.com>
2022-06-29 15:55:44 +0200

    Milestone 2 corretta

--- script.js
+++ script.js
@@ -80,8 +80,3 @@ previousBtn.addEventListener(`click`, function() {
 // Milestone 2:
 // Aggiungere il ciclo infinito del carosello. Ovvero se la miniatura attiva è la prima e l'utente clicca la freccia verso destra, 
 // la miniatura che deve attivarsi sarà l'ultima e viceversa per l'ultima miniatura se l'utente clicca la freccia verso sinistra.
-
-const clock = setInterval(function() {
-    checkright();
-    visualizeImage(index);
-}, 3000);
